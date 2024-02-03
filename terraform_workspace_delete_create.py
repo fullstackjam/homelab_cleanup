abd12303e4aecf364c0b4df9e937c519868a9143
brimdor <chrisnelsonx@gmail.com>
2024-02-03 13:34:08 -0600

    Update terraform_workspace_delete_create.py

--- terraform_workspace_delete_create.py
+++ terraform_workspace_delete_create.py
@@ -11,6 +11,8 @@ TERRAFORM_IO_API_TOKEN = os.environ.get("TERRAFORM_IO_API_TOKEN")
 VCS_IDENTIFIER = os.environ.get("VCS_IDENTIFIER")
 TERRAFORM_OAUTH_TOKEN_ID = os.environ.get("TERRAFORM_OAUTH_TOKEN_ID")
 REPO_TOKEN = os.environ.get("REPO_TOKEN")
+BRANCH_NAME = os.environ.get("BRANCH_NAME")
+
 
 def check_workspace_exists():
     url = "https://app.terraform.io/api/v2/organizations/{}/workspaces".format(ORGANIZATION)
@@ -72,40 +74,42 @@ def delete_workspace(workspace_name):
         print("")
 
 def create_workspace():
-    create_url = "https://app.terraform.io/api/v2/organizations/{}/workspaces".format(ORGANIZATION)
+    create_url = f"https://app.terraform.io/api/v2/organizations/{ORGANIZATION}/workspaces"
     headers = {
-        "Authorization": "Bearer {}".format(TERRAFORM_IO_API_TOKEN),
+        "Authorization": f"Bearer {TERRAFORM_IO_API_TOKEN}",
         "Content-Type": "application/vnd.api+json"
     }
+
     create_payload = {
         "data": {
             "attributes": {
                 "name": "homelab-external",
-                "execution-mode": "local",
-                "vcs-repo": {
-                    "identifier": VCS_IDENTIFIER,
-                    "oauth-token-id": TERRAFORM_OAUTH_TOKEN_ID,
-                    "branch": "main"
-                }
+                "execution-mode": "local"
             },
             "type": "workspaces"
         }
     }
 
+    # Conditionally add VCS repo information
+    if VCS_IDENTIFIER and TERRAFORM_OAUTH_TOKEN_ID:
+        create_payload["data"]["attributes"]["vcs-repo"] = {
+            "identifier": VCS_IDENTIFIER,
+            "oauth-token-id": TERRAFORM_OAUTH_TOKEN_ID,
+            "branch": BRANCH_NAME
+        }
+
     create_response = requests.post(create_url, headers=headers, json=create_payload)
 
     if create_response.status_code == 201:
-        print("")
-        print("><><><><><><><><><><><><><><><><><><><><><><><><>")
+        print("\n><><><><><><><><><><><><><><><><><><><><><><><><>")
         print("Workspace 'homelab-external' created successfully")
-        print("><><><><><><><><><><><><><><><><><><><><><><><><>")
-        print("")
+        print("><><><><><><><><><><><><><><><><><><><><><><><><>\n")
     else:
-        print("")
-        print("<><><><><><><><><><><><><><><>")
-        print("Error creating workspace: {}".format(create_response.status_code))
-        print("<><><><><><><><><><><><><><><>")
-        print("")
+        print("\n<><><><><><><><><><><><><><><>")
+        print(f"Error creating workspace: {create_response.status_code}")
+        print("Response: ", create_response.text)
+        print("<><><><><><><><><><><><><><><>\n")
+
 
 # Call the function to check and perform the necessary actions
 check_workspace_exists()
